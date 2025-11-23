942ae37d26391ddcb088bdb9f44fd2a0853e0db9
cryptanu <tripleatekh@gmail.com>
2025-11-23 07:58:18 -0300

    resolving vercel dep issues

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@deconcierge/services"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      "pino": "pino/browser",
+      "pino-pretty": false,
+      "lokijs": false,
+      "encoding": false,
+      "supports-color": false,
+      "thread-stream": false,
+      "sonic-boom": false,
+      "fastbench": false,
+      "pino-elasticsearch": false,
+      "tap": false,
+      "tape": false,
+      "desm": false,
+      "why-is-node-running": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
